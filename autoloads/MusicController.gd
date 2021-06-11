792ae61af7e81a8a87959774a93b42417222990d
Trevor Swope <pcllab@dmcinfo.com>
2021-06-11 14:22:18 -0700

    add placeholder audio player

---
+++ autoloads/MusicController.gd
@@ -0,0 +1,15 @@
+extends Node
+
+var main_music = load("res://resources/music/placeholder.wav")
+# Declare member variables here. Examples:
+# var a = 2
+# var b = "text"
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	pass # Replace with function body.
+
+func play_music():
+	$Music.stream = main_music
+	$Music.play()
